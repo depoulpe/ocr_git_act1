3178f942463346d46b1259a5de4afb653dd364f1
depoulpe <gilles.grenier@polypus.fr>
2018-11-28 11:22:55 +1100

    amélioration: ajout de l'heure

--- date.js
+++ date.js
@@ -1,4 +1,4 @@
-function date(id)
+function datetime(id)
 {
    date = new Date;
    year = date.getFullYear();
@@ -7,7 +7,22 @@ function date(id)
    d = date.getDate();
    day = date.getDay();
    days = new Array('Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi');
-   result = 'Nous sommes le '+days[day]+' '+d+' '+months[month]+' '+year;
+   h = date.getHours();
+   if(h<10)
+   {
+      h = "0"+h;
+   }
+   m = date.getMinutes();
+   if(m<10)
+      {
+         m = "0"+m;
+      }
+   s = date.getSeconds();
+   if(s<10)
+      {
+         s = "0"+s;
+      }
+   result = 'Nous sommes le '+days[day]+' '+d+' '+months[month]+' '+year+' il est '+h+':'+m+':'+s;
    document.getElementById(id).innerHTML = result;
    return result;     
 }
